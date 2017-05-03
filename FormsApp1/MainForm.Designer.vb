e136aa5a88a52928b026f59d6c81a7d73c4cf58d
Phil Wood <phil@pwood.net>
2017-05-03 16:52:11 +0100

    First commit

---
+++ FormsApp1/MainForm.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OKButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OKButton
+        '
+        Me.OKButton.Location = New System.Drawing.Point(335, 222)
+        Me.OKButton.Name = "OKButton"
+        Me.OKButton.Size = New System.Drawing.Size(75, 23)
+        Me.OKButton.TabIndex = 0
+        Me.OKButton.Text = "OK"
+        Me.OKButton.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(460, 275)
+        Me.Controls.Add(Me.OKButton)
+        Me.Name = "MainForm"
+        Me.Text = "Main Form"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents OKButton As System.Windows.Forms.Button
+
+End Class
---
+++ FormsApp1/MainForm.vb
@@ -0,0 +1,7 @@
+﻿Public Class MainForm
+
+    Private Sub OKButton_Click(sender As Object, e As EventArgs) Handles OKButton.Click
+        MsgBox("Hello")
+
+    End Sub
+End Class
